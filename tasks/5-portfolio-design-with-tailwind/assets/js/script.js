f050757aa1c8f24a2c2152de1fe1ace3a37e7a8a
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-05-28 11:27:37 +0530

    finally corrected the timing functionality

--- tasks/5-portfolio-design-with-tailwind/assets/js/script.js
+++ tasks/5-portfolio-design-with-tailwind/assets/js/script.js
@@ -8,20 +8,22 @@
         // console.log(dt);
         // console.log(dt.getHours()%12);
         // console.log(dt.getMinutes());
-        var time = dt.getHours() % 12 + ":" + dt.getMinutes();
+        var minutes = dt.getMinutes().toString().padStart(2, '0');
+        var time = dt.getHours() % 12 + ":" + minutes;
         // console.log(time);
-
+        
         dt.getHours() % 12 < 0 ? time += " PM" : time += " AM";
         // console.log(time);
-
+        
         document.querySelector(".time").textContent = time;
     }
-
+    
     function changeDateAndtime() {
         var dnt = new Date();
-        var str = dnt.getDate() + "/" + (dnt.getMonth() + 1) + "/" + dnt.getFullYear() + "   " + "[ " + dnt.getHours() % 12 + ":" + dnt.getMinutes();
+        var minutes = dnt.getMinutes().toString().padStart(2, '0');
+        var str = dnt.getDate() + "/" + (dnt.getMonth() + 1) + "/" + dnt.getFullYear() + "   " + "[ " + (dnt.getHours() % 12 || 12) + ":" + minutes;
 
-        dnt.getHours() % 12 > 0 ? str += " PM" + " ]" : str += " AM" + " ]";
+        dnt.getHours() % 12 < 0 ? str += " PM" + " ]" : str += " AM" + " ]";
 
         document.querySelector(".date-and-time").textContent = str;
     }
